529149de8c26019e760fd5c6a1da420823a2ff07
poikaa <82894118+poikaa@users.noreply.github.com>
2021-06-25 15:05:44 +0200

    static website

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,28 @@
+import * as path from "path";
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "iac-research-cdk", {
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
       bucketName: "iac-research-cdk-130359",
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [
+        s3deploy.Source.asset(path.join(__dirname, "../../app/client/build")),
+      ],
+      destinationBucket: websiteBucket,
+    });
+
+    new cdk.CfnOutput(this, "bucketWebsiteUrl", {
+      value: websiteBucket.bucketWebsiteUrl,
     });
   }
 }
