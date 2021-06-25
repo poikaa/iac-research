32ea54d6b63a7ec92b844113694b2f83bc201145
poikaa <82894118+poikaa@users.noreply.github.com>
2021-06-25 14:24:49 +0200

    create bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,12 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, "iac-research-cdk", {
+      bucketName: "iac-research-cdk-130359",
+    });
   }
 }
